d4909d3be16ad4fa82299f98563378ddf9ebd74a
Conner Ahn <32886925+HK-An@users.noreply.github.com>
2022-05-30 17:08:11 +0900

    테스트를 위한 공통 코드 추가

---
+++ src/test/java/kr/hh/liverary/common/interfaces/CrudInterface.java
@@ -0,0 +1,9 @@
+package kr.hh.liverary.common.interfaces;
+
+public interface CrudInterface {
+
+    public void test_create() throws Exception;
+    public void test_modify() throws Exception;
+    public void test_findAll() throws Exception;
+    public void test_remove() throws Exception;
+}
---
+++ src/test/java/kr/hh/liverary/common/interfaces/TestInterface.java
@@ -0,0 +1,14 @@
+package kr.hh.liverary.common.interfaces;
+
+
+public interface TestInterface {
+
+    public void tearDown();
+
+
+    ///////////////////////////
+    public String userName = "John Doe";
+    public String userMail = "mymail@mail.com";
+    public String pictureUrl = "http://picture.com/photo/1";
+
+}
